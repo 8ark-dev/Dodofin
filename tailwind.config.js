88df40662003544fe93bb98eb3f24dfca5009f8f
8ark <sorlti6952@gmail.com>
2023-03-22 10:34:13 +0900

    init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{ts,tsx,js,jsx}' , './modules/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
